da74b094502ee093d907bbdf5c76c30de6a8cabc
Joshua Zeldin <joshzeldin1@gmail.com>
2020-03-15 13:51:16 -0400

    changes scrolling behavior on map

--- resources/public/js/svg-map.js
+++ resources/public/js/svg-map.js
@@ -1,6 +1,9 @@
 d3.selectAll("circle").style("opacity", 0.0)
 
-d3.select("svg").call(d3.zoom().on("zoom", function () {
+zoomBaseElement = d3.select("svg")
+
+zoomBaseElement.call(d3.zoom().on("zoom", function () {
+    console.log(d3.event.transform)
     d3.select("g.map").attr("transform", d3.event.transform)
  }))
 
@@ -10,12 +13,15 @@ var trekScroll = window.requestAnimationFrame ||
     };
 
 function arrivedAtLocation() {
-    cities = d3.selectAll("g.trek").selectAll("circle")
+    cities = d3.selectAll("circle.trek.village")
     cities.style("opacity", function(d, i) {
+        //console.log(d)
         scrollPercent = getScrollPercent();
-        trekDistance = i / cities.size();
+        trekDistance = (i + 1)/ cities.size();
         return trekDistance > scrollPercent ? 0.0 : 1.0;
         });
+    
+    //d3.select("g.map").attr("transform", "translate(0,0)")
     trekScroll(arrivedAtLocation)
 }
 arrivedAtLocation()
